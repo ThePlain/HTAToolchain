a33129f34f87bfdc1679bbdf68b5ac7454597946
Alexander Fateev <foggy1989@gmail.com>
2021-01-19 23:20:47 +0300

    Added blender interface and property prototype

--- __init__.py
+++ __init__.py
@@ -5,13 +5,14 @@ import bpy.utils
 import bpy_extras.io_utils
 
 from . import TargemImport
+from . import TargemInterface
 
 
 bl_info = {
     "name": "Targem Files Toolchain",
     "blender": (2, 80, 0),
     "category": 'Import-Export',
-    "version": (1, 0, 1),
+    "version": (1, 1, 17),
     "desctiption": "Import Targem files",
     "support": 'TESTING',
     "author": 'ThePlain (Alexander Fateev)',
@@ -28,11 +29,21 @@ def menu_func_import(self, context):
     )
 
 def register():
+    bpy.utils.register_class(TargemInterface.HTAConfig)
+    bpy.utils.register_class(TargemInterface.HTAObjectProperty)
+    bpy.utils.register_class(TargemInterface.HTAObjectPanel)
+
     bpy.utils.register_class(TargemImport.ImportGAMFile)
     bpy.utils.register_class(TargemImport.ImportSAMFile)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
+    bpy.types.Object.hta_config = bpy.props.PointerProperty(type=TargemInterface.HTAObjectProperty)
+
 def unregister():
+    bpy.utils.unregister_class(TargemInterface.HTAConfig)
+    bpy.utils.unregister_class(TargemInterface.HTAObjectProperty)
+    bpy.utils.unregister_class(TargemInterface.HTAObjectPanel)
+
     bpy.utils.unregister_class(TargemImport.ImportGAMFile)
     bpy.utils.unregister_class(TargemImport.ImportSAMFile)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
